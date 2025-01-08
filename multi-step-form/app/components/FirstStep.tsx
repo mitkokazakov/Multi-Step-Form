0ff76754a6311a6b4947712abb626eb6148d25ee
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2025-01-08 13:44:15 +0200

    Styling first step and steps navigation for large screens and mobile devices

--- multi-step-form/app/components/FirstStep.tsx
+++ multi-step-form/app/components/FirstStep.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FirstStep = () => {
   return (
-    <div className="w-full h-full relative bg-gray-200 lg:flex lg:flex-col lg:justify-between items-center">
+    <div className="w-full h-full relative bg-gray-200 lg:flex lg:flex-col lg:justify-between items-center lg:bg-white">
       <div className="w-[90%] left-[50%] bg-white rounded-xl translate-x-[-50%] absolute top-[-10%] flex flex-col justify-center items-start gap-5 px-5 py-10 lg:static lg:translate-x-[0%] lg:w-full">
         <h1 className="text-3xl font-extrabold tracking-widest">
           Personal Info
@@ -44,7 +44,7 @@ const FirstStep = () => {
         </div>
       </div>
 
-      <div className="w-full h-24 absolute bg-white bottom-0 flex justify-end items-center lg:static">
+      <div className="w-full h-24 absolute bg-white bottom-0 flex justify-end items-center lg:static lg:px-5">
         <div className="w-[90%]  flex justify-end items-center mx-auto lg:w-full">
           <button className="bg-blue-950 text-white text-xl px-6 py-2 rounded-md lg:text-base">
             Next Step
--- multi-step-form/app/components/StepsNavigation.tsx
+++ multi-step-form/app/components/StepsNavigation.tsx
@@ -2,8 +2,48 @@ import React from 'react'
 
 const StepsNavigation = () => {
   return (
-    <div className='h-56 w-full steps-navigation lg:h-full lg:w-[40%] lg:rounded-xl'>
-      
+    <div className='h-56 w-full steps-navigation lg:h-full lg:w-[40%] lg:rounded-xl lg:pl-10'>
+      <div className='w-full flex justify-center items-start gap-10 py-10 lg:flex-col'>
+        <div className='flex justify-center items-center gap-5'>
+            <button className='w-10 h-10 rounded-full bg-transparent border-[1px] border-white text-white font-bold'>1</button>
+
+            <div className=' hidden lg:flex lg:flex-col lg:justify-center lg:items-start lg:text-white'>
+                <p className='text-sm text-gray-400'>STEP 1</p>
+
+                <h4 className='text-sm font-bold tracking-widest'>YOUR INFO</h4>
+            </div>
+        </div>
+
+        <div className='flex justify-center items-center gap-5'>
+            <button className='w-10 h-10 rounded-full bg-transparent border-[1px] border-white text-white font-bold'>2</button>
+
+            <div className=' hidden lg:flex lg:flex-col lg:justify-center lg:items-start lg:text-white'>
+                <p className='text-sm text-gray-400'>STEP 2</p>
+
+                <h4 className='text-sm font-bold tracking-widest'>SELECT PLAN</h4>
+            </div>
+        </div>
+
+        <div className='flex justify-center items-center gap-5'>
+            <button className='w-10 h-10 rounded-full bg-transparent border-[1px] border-white text-white font-bold'>3</button>
+
+            <div className=' hidden lg:flex lg:flex-col lg:justify-center lg:items-start lg:text-white'>
+                <p className='text-sm text-gray-400'>STEP 3</p>
+
+                <h4 className='text-sm font-bold tracking-widest'>ADD-ONS</h4>
+            </div>
+        </div>
+
+        <div className='flex justify-center items-center gap-5'>
+            <button className='w-10 h-10 rounded-full bg-transparent border-[1px] border-white text-white font-bold'>4</button>
+
+            <div className=' hidden lg:flex lg:flex-col lg:justify-center lg:items-start lg:text-white'>
+                <p className='text-sm text-gray-400'>STEP 4</p>
+
+                <h4 className='text-sm font-bold tracking-widest'>SUMMARY</h4>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
