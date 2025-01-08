695fc97c72ca01d590cc2442e77852c5d86546bf
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2025-01-08 09:46:52 +0200

    Initial commit

---
+++ multi-step-form/app/components/FormWrapper.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const FormWrapper = () => {
+  return (
+    <div className='w-[700px] h-[500px] bg-white'>
+      
+    </div>
+  )
+}
+
+export default FormWrapper
---
+++ multi-step-form/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import FormWrapper from "./components/FormWrapper";
+
+export default function Home() {
+  return (
+    <div className="h-screen bg-gray-200 flex items-center justify-center">
+      <FormWrapper />
+    </div>
+  );
+}
